5eb20121e8f92f14909ee4fed3692ac2a3217fea
steamfaffa@gmail.com <>
2023-02-13 19:31:31 +0200

    Made Inspector Better

---
+++ Assets/Scripts/Runtime/GameManager/GameManager.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using Runtime.Player;
+
+namespace Runtime.GameManager
+{
+    public class GameManager : MonoBehaviour
+    {
+        public static readonly GameManager Instance = new();
+        
+        
+    }
+}
