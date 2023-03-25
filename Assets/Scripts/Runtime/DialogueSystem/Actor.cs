5ee648824a0c2230958012e7081559c9870e58c5
francois-smith <steamfaffa@gmail.com>
2023-03-25 14:12:54 +0200

    Fixed Corrupted sln

--- Assets/Scripts/Runtime/DialogueSystem/Actor.cs
+++ Assets/Scripts/Runtime/DialogueSystem/Actor.cs
@@ -1,11 +1,14 @@
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "New Actor", menuName = "Dialogue System/Actor")]
-public class Actor : ScriptableObject
+namespace Runtime.DialogueSystem
 {
-    [SerializeField] private string _name;
-    [SerializeField] private Sprite _sprite;
-
-    public string Name => _name;
-    public Sprite Sprite => _sprite;
+    [CreateAssetMenu(fileName = "New Actor", menuName = "Dialogue System/Actor")]
+    public class Actor : ScriptableObject
+    {
+        [SerializeField] private new string name;
+        [SerializeField] private Sprite sprite;
+    
+        public string Name => name;
+        public Sprite Sprite => sprite;
+    }
 }
