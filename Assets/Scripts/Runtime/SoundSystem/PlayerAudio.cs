aad66ed6529e780470cf1ea63a2c20b9ec789830
francois-smith <steamfaffa@gmail.com>
2023-05-03 17:23:10 +0200

    Added sound particles

--- Assets/Scripts/Runtime/SoundSystem/PlayerAudio.cs
+++ Assets/Scripts/Runtime/SoundSystem/PlayerAudio.cs
@@ -1,47 +1,69 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
-public class PlayerAudio : MonoBehaviour
+namespace Runtime.SoundSystem
 {
-    [Header("REFERENCES")]
-    [SerializeField] private AudioSource audioSource;
-    
-    [Header("SOUNDS")]
-    [SerializeField] private List<AudioClip> concreteFootsteps;
-    [SerializeField] private List<AudioClip> waterFootsteps;
-    
-    [Header("DEBUG")]
-    [SerializeField] private bool debug;
-    [SerializeField] private ParticleSystem noiseEffect;
-    
-    public void PlayFootstepSound()
+    public class PlayerAudio : MonoBehaviour
     {
-        var raycastHit2D = Physics2D.Raycast(transform.position, new Vector3(0f, 0f, -1f), 1f);
-        if (raycastHit2D.collider != null)
+        [Header("REFERENCES")]
+        [SerializeField] private AudioSource audioSource;
+    
+        [Header("SOUNDS")]
+        [SerializeField] private List<AudioClip> concreteFootsteps;
+        [SerializeField] private List<AudioClip> waterFootsteps;
+    
+        [Header("DEBUG")]
+        [SerializeField] private bool debug;
+        [SerializeField] private ParticleSystem noiseEffect;
+    
+        public void PlayFootstepSound()
         {
-            if (raycastHit2D.collider.CompareTag("Water"))
+            audioSource.volume = 0.5f;
+            audioSource.PlayOneShot(DetermineSound());
+            
+            if(debug) ShowDebugSound(4f);
+        }
+
+        public void PlaySneakSound()
+        {
+            audioSource.volume = 0.3f;
+            audioSource.PlayOneShot(DetermineSound());
+            
+            if(debug) ShowDebugSound(2.5f);
+        }
+
+        private AudioClip DetermineSound()
+        {
+            var raycastHit2D = Physics2D.Raycast(transform.position, new Vector3(0f, 0f, -1f), 1f);
+            if (raycastHit2D.collider != null)
             {
-                audioSource.PlayOneShot(waterFootsteps[Random.Range(0, waterFootsteps.Count)]);
+                if (raycastHit2D.collider.CompareTag("Water"))
+                {
+                    return waterFootsteps[Random.Range(0, waterFootsteps.Count)];
+                }
+                else if (raycastHit2D.collider.CompareTag("Concrete"))
+                {
+                    return concreteFootsteps[Random.Range(0, concreteFootsteps.Count)];
+                }
             }
-            else if (raycastHit2D.collider.CompareTag("Concrete"))
+
+            return null;
+        }
+
+        private void ShowDebugSound(float range)
+        {
+            if(debug)
             {
-                audioSource.PlayOneShot(concreteFootsteps[Random.Range(0, concreteFootsteps.Count)]);
+                if (noiseEffect != null)
+                {
+                    var newParticleSystem = Instantiate(noiseEffect, audioSource.transform.position, Quaternion.Euler(90, 0,0 ));
+                    newParticleSystem.transform.localScale = new Vector3(range, range, range);
+                    newParticleSystem.Play();
+                    Destroy(newParticleSystem.gameObject, 2f);
+                }
             }
         }
         
-        if(debug)
-        {
-            if (noiseEffect != null)
-            {
-                var newParticleSystem = Instantiate(noiseEffect, audioSource.transform.position, Quaternion.identity);
-                var mainModule = newParticleSystem.main;
-                mainModule.startSize = 10f;
-                newParticleSystem.Play();
-                Destroy(newParticleSystem.gameObject, 4f);
-            }
-        }
     }
 }
