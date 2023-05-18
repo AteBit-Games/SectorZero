b1c6044493e86c09f29f8d23780c13e8e29a30f3
francois-smith <steamfaffa@gmail.com>
2023-05-18 19:10:10 +0200

    Fixed Condition Node

--- Assets/Scripts/Runtime/BehaviourTree/Conditions/Navigation/CheckValidPoint.cs
+++ Assets/Scripts/Runtime/BehaviourTree/Conditions/Navigation/CheckValidPoint.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using Runtime.Navigation;
 using UnityEngine;
 
 namespace Runtime.BehaviourTree.Conditions.Navigation
@@ -10,7 +9,7 @@ namespace Runtime.BehaviourTree.Conditions.Navigation
     [Description("Check if the agent has a valid path")]
     public class CheckValidPoint : ConditionNode
     {
-        public new NodeProperty<Vector2> position;
+        public new NodeProperty<Vector2> positionToCheck;
 
         protected override void OnStart() { }
 
@@ -19,7 +18,7 @@ namespace Runtime.BehaviourTree.Conditions.Navigation
         protected override State OnUpdate()
         {
             if(!context.agent.map) return State.Failure;
-            return context.agent.map.PointIsValid(position.Value) ? State.Success : State.Failure;
+            return context.agent.map.PointIsValid(positionToCheck.Value) ? State.Success : State.Failure;
         }
 
         protected override void OnReset() { }
