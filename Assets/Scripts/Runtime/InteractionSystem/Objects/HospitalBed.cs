ced8b8524ec180e99c77480660d2f05ead037262
steamfaffa@gmail.com <>
2023-07-20 14:18:51 +0200

    Changed Pixel Ratio Tutorial Room Building Blocks

---
+++ Assets/Scripts/Runtime/InteractionSystem/Objects/HospitalBed.cs
@@ -0,0 +1,60 @@
+/****************************************************************
+* Copyright (c) 2023 AteBit Games
+* All rights reserved.
+****************************************************************/
+
+using System;
+using Runtime.InteractionSystem.Interfaces;
+using Runtime.Managers;
+using Runtime.Player;
+using Runtime.SoundSystem.ScriptableObjects;
+using UnityEngine;
+
+namespace Runtime.InteractionSystem.Objects
+{
+    public class HospitalBed : MonoBehaviour, IInteractable
+    {
+        [SerializeField] private Sound interactSound;
+        public Sound InteractSound => interactSound;
+        
+        private Collider2D _interactionCollider;
+
+        private bool active;
+
+        private void Awake()
+        {
+            TutorialManager.StartListening("TutorialStage2", Init);
+            _interactionCollider = GetComponent<Collider2D>();
+            
+            // Disable interaction
+            gameObject.layer = 0;
+            _interactionCollider.enabled = false;
+        }
+
+        public bool OnInteract(GameObject player)
+        {
+            var nellient = player.GetComponentInParent<Nellient>();
+            nellient.StartGame();
+            Finish();
+            return true;
+        }
+
+        private void Init()
+        {
+            active = true;
+            gameObject.layer = 3;
+            _interactionCollider.enabled = true;
+        }
+        
+        private void Finish()
+        {
+            active = false;
+            gameObject.layer = 0;
+        }
+        
+        public bool CanInteract()
+        {
+            return active;
+        }
+    }
+}
--- Assets/Scripts/temp.cs
+++ Assets/Scripts/temp.cs
@@ -1,19 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
-using Runtime.AI;
+
+using Runtime.Managers;
 using UnityEngine;
+
 
 public class temp : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        GetComponent<NoiseEmitter>().EmitLocal();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   
 }
