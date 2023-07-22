106a7d04f86a5ccd3aefddb137f631340d6a530b
steamfaffa@gmail.com <>
2023-07-22 13:41:12 +0200

    Upgraded Editor Version

--- Assets/Scripts/temp.cs
+++ Assets/Scripts/temp.cs
@@ -1,9 +1,13 @@
 
+using System;
+using ElRaccoone.Tweens;
 using Runtime.Managers;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 
 public class temp : MonoBehaviour
 {
-   
+
 }
