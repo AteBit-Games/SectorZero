bc951c522cb63e7e167c9ecb7072412f914dd269
rosstordiffe24@gmail.com <>
2023-08-01 21:29:03 +0200

    Door, new walls and light

--- Assets/Scripts/Runtime/InteractionSystem/Objects/TriggerDoor.cs
+++ Assets/Scripts/Runtime/InteractionSystem/Objects/TriggerDoor.cs
@@ -36,6 +36,7 @@ namespace Runtime.InteractionSystem.Objects
         
         public void OpenDoor()
         {
+            Debug.Log("Open door");
             GameManager.Instance.SoundSystem.Play(openSound, transform);
             _mainAnimator.SetBool(_isOpen, true);
         }
