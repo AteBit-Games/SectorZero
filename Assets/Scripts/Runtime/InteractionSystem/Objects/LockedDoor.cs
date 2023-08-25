5d5bdca770f2bf616d48772648a340b22fdee299
Ross-Tordiffe <rosstordiffe24@gmail.com>
2023-08-25 14:16:14 +0200

    Started setting up door interactions

--- Assets/Scripts/Runtime/InteractionSystem/Objects/LockedDoor.cs
+++ Assets/Scripts/Runtime/InteractionSystem/Objects/LockedDoor.cs
@@ -7,8 +7,8 @@ using Runtime.InteractionSystem.Interfaces;
 using Runtime.InventorySystem.ScriptableObjects;
 using Runtime.Managers;
 using Runtime.SoundSystem;
-using Unity.AI.Navigation;
 using UnityEngine;
+using NavMeshPlus.Components;
 
 namespace Runtime.InteractionSystem.Objects
 {
@@ -26,7 +26,10 @@ namespace Runtime.InteractionSystem.Objects
         [SerializeField] private NavMeshSurface navMeshSurface;
         
         private Animator _animator;
-        private static readonly int OpenDoor = Animator.StringToHash("OpenDoor");
+        private readonly int _isOpen = Animator.StringToHash("isOpen");
+        private static readonly int Locked = Animator.StringToHash("locked");
+
+
 
         //========================= Unity Events =========================//
 
@@ -40,13 +43,14 @@ namespace Runtime.InteractionSystem.Objects
         public bool OnInteract(GameObject player)
         {
             GameManager.Instance.SoundSystem.Play(interactSound, transform.GetComponent<AudioSource>());
-            _animator.SetTrigger(OpenDoor);
+            _animator.SetBool(_isOpen, true);
             return true;
         }
 
         public void OnInteractFailed(GameObject player)
         {
             GameManager.Instance.SoundSystem.Play(lockedSound, transform.GetComponent<AudioSource>());
+            _animator.SetTrigger(Locked);
         }
 
         public bool CanInteract()
