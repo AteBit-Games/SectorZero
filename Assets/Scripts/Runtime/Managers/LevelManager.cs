57115a32c26f540620e9b13b3954a4b107536de2
francois-smith <steamfaffa@gmail.com>
2023-08-26 11:26:56 +0200

    Culling Demo

--- Assets/Scripts/Runtime/Managers/LevelManager.cs
+++ Assets/Scripts/Runtime/Managers/LevelManager.cs
@@ -4,40 +4,84 @@
 ****************************************************************/
 
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine.SceneManagement;
 using UnityEngine;
 using NavMeshPlus.Components;
-using Runtime.SoundSystem;
+using Runtime.Misc;
 
 namespace Runtime.Managers
 {
+    [Serializable]
+    public class CullingSection
+    {
+        public string tag;
+        public List<CullingTarget> cullingTargets;
+    }
+
+    [Serializable]
+    public class CullingTarget
+    {
+        public Transform targetCenter;
+        public float radius;
+        public List<CustomLight> lights;
+    }
+    
     public class LevelManager : MonoBehaviour
     {
-        [SerializeField] private Camera mainCamera;
         [SerializeField] private string levelDescription;
         [SerializeField] private NavMeshSurface navMeshSurface;
+        [SerializeField] private Transform cullingReferencePoint;
+        public List<CullingSection> targets;
 
-        private  CullingGroup _cullingGroup;
-        private BoundingSphere[] _boundingSpheres;
-        private float _playerRadius = 1f;
+        private CullingGroup cullingGroup;
+        private BoundingSphere[] bounds;
+        private CullingTarget[] cullingTargets;
+
         
         private void Awake()
         {
+            cullingGroup = new CullingGroup();
+
+            cullingGroup.targetCamera = Camera.main;
+            cullingGroup.SetDistanceReferencePoint(cullingReferencePoint);
+            cullingGroup.SetBoundingDistances(new[] { 10.0f });
+
+            int count = targets.Sum(section => section.cullingTargets.Count);
+
+            int index = 0;
+            bounds = new BoundingSphere[count];
+            cullingTargets = new CullingTarget[count];
+            foreach (var target in targets.SelectMany(section => section.cullingTargets))
+            {
+                bounds[index] = new BoundingSphere(target.targetCenter.position, target.radius);
+                cullingTargets[index] = target;
+                index++;
+            }
+
+            cullingGroup.SetBoundingSpheres(bounds);
+            cullingGroup.SetBoundingSphereCount(count);
+
+            foreach (var customLight in targets.SelectMany(section => section.cullingTargets.SelectMany(target => target.lights)))
+            {
+                customLight.gameObject.SetActive(false);
+            }
+
+            cullingGroup.onStateChanged = OnChange;
             SceneManager.sceneLoaded += SceneLoaded;
-            _cullingGroup = new CullingGroup();
-            _cullingGroup.targetCamera = mainCamera;
-            _boundingSpheres = new BoundingSphere[25];
-            _boundingSpheres[0] = new BoundingSphere(Vector3.zero, 10f);
-            _cullingGroup.SetBoundingSpheres(_boundingSpheres);
-            _cullingGroup.onStateChanged = StateChangedMethod;
         }
         
-        private void StateChangedMethod(CullingGroupEvent evt)
+        private void OnChange(CullingGroupEvent group)
         {
-            if(evt.hasBecomeVisible)
-                Debug.LogFormat("Sphere {0} has become visible!", evt.index);
-            if(evt.hasBecomeInvisible)
-                Debug.LogFormat("Sphere {0} has become invisible!", evt.index);
+            if(group.hasBecomeVisible)
+            {
+                foreach (var customLight in cullingTargets[group.index].lights) customLight.gameObject.SetActive(true);
+            }
+            else if(group.hasBecomeInvisible)
+            {
+                foreach (var customLight in cullingTargets[group.index].lights) customLight.gameObject.SetActive(false);
+            }
         }
 
         private void Start()
@@ -49,12 +93,8 @@ namespace Runtime.Managers
 
         private void OnDestroy()
         {
-            _cullingGroup.Dispose();
-        }
-
-        private void Update()
-        {
-            
+            cullingGroup.Dispose();
+            cullingGroup = null;
         }
 
         private void SceneLoaded(Scene scene, LoadSceneMode mode)
@@ -64,8 +104,16 @@ namespace Runtime.Managers
 
         private void OnDrawGizmos()
         {
+            if (targets == null) return;
             Gizmos.color = Color.red;
-            Gizmos.DrawWireSphere(Vector3.zero, 10f);
+            for (int i = 0; i < targets.Count; i++)
+            {
+                foreach (var target in targets[i].cullingTargets)
+                {
+                    if(target.targetCenter == null) continue;
+                    Gizmos.DrawWireSphere(target.targetCenter.position, target.radius);
+                }
+            }
         }
     }
 }
