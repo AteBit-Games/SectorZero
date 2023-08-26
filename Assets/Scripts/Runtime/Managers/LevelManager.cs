a74a6af3a6c38d260bfda4a20ff9249227bde7c9
francois-smith <steamfaffa@gmail.com>
2023-08-26 14:11:01 +0200

    Reworking wall colliders

--- Assets/Scripts/Runtime/Managers/LevelManager.cs
+++ Assets/Scripts/Runtime/Managers/LevelManager.cs
@@ -17,11 +17,24 @@ namespace Runtime.Managers
     public class CullingSection
     {
         public string tag;
-        public List<CullingTarget> cullingTargets;
     }
 
     [Serializable]
-    public class CullingTarget
+    public class LightCullingSection : CullingSection
+    {
+        public List<CullingLight> cullingLightTargets;
+    }
+    
+    [Serializable]
+    public class WallCullingSection : CullingSection
+    {
+        public Transform targetCenter;
+        public float radius;
+        public GameObject cullingWallContainer;
+    }
+
+    [Serializable]
+    public class CullingLight
     {
         public Transform targetCenter;
         public float radius;
@@ -33,39 +46,65 @@ namespace Runtime.Managers
         [SerializeField] private string levelDescription;
         [SerializeField] private NavMeshSurface navMeshSurface;
         [SerializeField] private Transform cullingReferencePoint;
-        public List<CullingSection> targets;
+        public List<LightCullingSection> lightTargets;
+        public List<WallCullingSection> wallTargets;
 
         private CullingGroup cullingGroup;
         private BoundingSphere[] bounds;
-        private CullingTarget[] cullingTargets;
+        private Dictionary<int, CullingSection> cullingTargets;
 
-        
         private void Awake()
         {
             cullingGroup = new CullingGroup();
 
             cullingGroup.targetCamera = Camera.main;
             cullingGroup.SetDistanceReferencePoint(cullingReferencePoint);
-            cullingGroup.SetBoundingDistances(new[] { 10.0f });
+            cullingGroup.SetBoundingDistances(new[] { 25.0f });
 
-            int count = targets.Sum(section => section.cullingTargets.Count);
+            int count = lightTargets.Sum(section => section.cullingLightTargets.Count);
+            count += wallTargets.Count;
+            cullingTargets = new Dictionary<int, CullingSection>(count);
 
             int index = 0;
             bounds = new BoundingSphere[count];
-            cullingTargets = new CullingTarget[count];
-            foreach (var target in targets.SelectMany(section => section.cullingTargets))
+            foreach (var section in lightTargets)
             {
-                bounds[index] = new BoundingSphere(target.targetCenter.position, target.radius);
-                cullingTargets[index] = target;
+                foreach (var target in section.cullingLightTargets)
+                {
+                    bounds[index] = new BoundingSphere(target.targetCenter.position, target.radius);
+                    cullingTargets.Add(index, section);
+                    index++;
+                }
+            }
+            
+
+            foreach (var section in wallTargets)
+            {
+                bounds[index] = new BoundingSphere(section.targetCenter.position, section.radius);
+                cullingTargets.Add(index, section);
                 index++;
             }
 
             cullingGroup.SetBoundingSpheres(bounds);
             cullingGroup.SetBoundingSphereCount(count);
 
-            foreach (var customLight in targets.SelectMany(section => section.cullingTargets.SelectMany(target => target.lights)))
+            foreach (var section in cullingTargets.Values)
             {
-                customLight.gameObject.SetActive(false);
+                if(section is LightCullingSection lightSection)
+                {
+                    foreach (var customLight in lightSection.cullingLightTargets.SelectMany(target => target.lights))
+                    {
+                        customLight.gameObject.SetActive(false);
+                    }
+                }
+                else if(section is WallCullingSection wallSection)
+                {
+                    for (var i = 0; i < wallSection.cullingWallContainer.gameObject.transform.childCount; i++)
+                    {
+                        var wall = wallSection.cullingWallContainer.gameObject.transform.GetChild(i);
+                        wall.gameObject.SetActive(false);
+                    }
+                }
             }
 
             cullingGroup.onStateChanged = OnChange;
@@ -76,11 +115,42 @@ namespace Runtime.Managers
         {
             if(group.hasBecomeVisible)
             {
-                foreach (var customLight in cullingTargets[group.index].lights) customLight.gameObject.SetActive(true);
+                var target = cullingTargets[group.index];
+                if(target is LightCullingSection lightSection)
+                {
+                    Debug.Log($"Target {target} is a light section");
+                    foreach (var customLight in lightSection.cullingLightTargets.SelectMany(cullingLight => cullingLight.lights))
+                    {
+                        customLight.gameObject.SetActive(true);
+                    }
+                }
+                else if(target is WallCullingSection wallSection)
+                {
+                    for (var i = 0; i < wallSection.cullingWallContainer.gameObject.transform.childCount; i++)
+                    {
+                        var wall = wallSection.cullingWallContainer.gameObject.transform.GetChild(i);
+                        wall.gameObject.SetActive(true);
+                    }
+                }
             }
             else if(group.hasBecomeInvisible)
             {
-                foreach (var customLight in cullingTargets[group.index].lights) customLight.gameObject.SetActive(false);
+                var target = cullingTargets[group.index];
+                if(target is LightCullingSection lightSection)
+                {
+                    foreach (var customLight in lightSection.cullingLightTargets.SelectMany(cullingLight => cullingLight.lights))
+                    {
+                        customLight.gameObject.SetActive(false);
+                    }
+                }
+                else if(target is WallCullingSection wallSection)
+                {
+                    for (var i = 0; i < wallSection.cullingWallContainer.gameObject.transform.childCount; i++)
+                    {
+                        var wall = wallSection.cullingWallContainer.gameObject.transform.GetChild(i);
+                        wall.gameObject.SetActive(false);
+                    }
+                }
             }
         }
 
@@ -102,17 +172,28 @@ namespace Runtime.Managers
             GameManager.Instance.details = levelDescription;
         }
 
+        
+        private readonly Color[] colors = {Color.red, Color.blue, Color.green, Color.yellow, Color.cyan, Color.magenta};
         private void OnDrawGizmos()
         {
-            if (targets == null) return;
-            Gizmos.color = Color.red;
-            for (int i = 0; i < targets.Count; i++)
+            
+            // if (lightTargets == null) return;
+            //
+            // var index = 0;
+            // foreach (var section in lightTargets)
+            // {
+            //     Gizmos.color = colors[index];
+            //     foreach (var target in section.cullingLightTargets)
+            //     {
+            //         Gizmos.DrawWireSphere(target.targetCenter.position, target.radius);
+            //     }
+            //     index++;
+            //}
+            
+            foreach (var section in wallTargets)
             {
-                foreach (var target in targets[i].cullingTargets)
-                {
-                    if(target.targetCenter == null) continue;
-                    Gizmos.DrawWireSphere(target.targetCenter.position, target.radius);
-                }
+                Gizmos.color = Color.white;
+                Gizmos.DrawWireSphere(section.targetCenter.position, section.radius);
             }
         }
     }
